5d04e723ef7a11f7dbd254cde14e5008359c406b
Daniel <jodagito@gmail.com>
2020-05-14 09:31:30 -0500

    feat: bettor class
    A way to manage bettors was required.
    
    Bettor class represents the users and has the following attributes:
    
    + int id
    + String name

---
+++ app/src/main/java/com/example/betrush/Bettor.java
@@ -0,0 +1,13 @@
+package com.example.betrush;
+
+public class Bettor {
+    final int id;
+    final String name;
+
+    public Bettor(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+}
+
+
